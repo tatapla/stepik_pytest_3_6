61a89e7ea0d7b80189688e3b29cee68250293283
unknown <tataspla@gmail.com>
2019-09-10 00:47:33 +0300

    fix test

--- test_items.py
+++ test_items.py
@@ -4,4 +4,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_basket_is_found(browser):
 	browser.get(link)
+	#time.sleep(10)
 	assert browser.find_element_by_class_name("btn-add-to-basket")
